5a23e500b36ede59604c654de350b3014e0f690e
Subin Erattakulangara <subinedu@gmail.com>
2024-07-29 16:31:33 -0500

    saving output image as artifact

--- MLFlowFineTune.py
+++ MLFlowFineTune.py
@@ -1,10 +1,12 @@
 # %%
-# Load the libraries
+# Import libraries
 import os
 import torch
 import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+import argparse
+import mlflow
 
 from monai.losses import DiceCELoss
 from monai.inferers import sliding_window_inference
@@ -29,10 +31,8 @@ from monai.transforms import (
     RandSpatialCropSamplesd,
     LambdaD
 )
-
 from monai.metrics import DiceMetric
 from monai.networks.nets import UNETR
-
 from monai.data import (
     DataLoader,
     CacheDataset,
@@ -40,143 +40,128 @@ from monai.data import (
     decollate_batch,
 )
 
-print_config()
+#print_config()
 
 # %%
-# Setup the parameters
-# Training Hyper-params
-lr = 1e-4
-max_iterations = 10000
-eval_num = 100
-experiment_name = "Test1"
+# Command-line argument parsing
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="Train UNETR model with MONAI")
+    parser.add_argument("--lr", type=float, default=1e-4, help="Learning rate")
+    parser.add_argument("--max_iterations", type=int,
+                        default=10000, help="Maximum number of iterations")
+    parser.add_argument("--eval_num", type=int, default=100,
+                        help="Number of iterations between evaluations")
+    parser.add_argument("--experiment_name", type=str,
+                        default="Test1", help="Name of the experiment")
+    parser.add_argument("--epochs", type=int, default=20,
+                        help="Number of epochs")
+    parser.add_argument("--dropout", type=float,
+                        default=0.0, help="Dropout rate")
+    parser.add_argument("--use_pretrained", default=True,action="store_true",
+                        help="Flag to use pretrained model")
+    parser.add_argument("--pretrained_path", type=str,
+                        default="./logs/Train-Thresh-255-2000EP-16th.pth", help="Path to the pretrained model")
+    parser.add_argument("--train_dir", type=str,
+                        default="./Data/FrenchSpeakerDataset/Segmentations/", help="Directory with training data")
+    parser.add_argument("--val_dir", type=str, default="./Data/FrenchSpeakerDataset/Segmentations_Val/",
+                        help="Directory with validation data")
+    return parser.parse_args()
+
+
+args = parse_arguments()
+
+# %%
+# Setup parameters and device
+lr = args.lr
+max_iterations = args.max_iterations
+eval_num = args.eval_num
+experiment_name = args.experiment_name
+epochs = args.epochs
+dropout = args.dropout
+use_pretrained = args.use_pretrained
+pretrained_path = args.pretrained_path
+train_dir = args.train_dir
+val_dir = args.val_dir
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Device used for processing {device}")
-#device = torch.device("cpu")
 
 # %%
-#setup the log directory
-logdir = os.path.normpath("./logs/Experiment1/")
-
-if os.path.exists(logdir) is False:
+# Setup log directory
+logdir = os.path.normpath(f"./logs/{experiment_name}/")
+if not os.path.exists(logdir):
     os.mkdir(logdir)
 
-# Load the pre-trained model
-use_pretrained = True
-pretrained_path = os.path.normpath("./logs/Train-Thresh-255-2000EP-16th.pth")
-
-
 # %%
-# Convert the train and validation images into a list with locations
-train_dir = "./Data/FrenchSpeakerDataset/Segmentations/"
-val_dir = "./Data/FrenchSpeakerDataset/Segmentations_Val/"
-
-# train image file
-# timage_filenames = sorted([os.path.join(train_dir, f) for f in os.listdir(train_dir) if f.endswith("im")])
+# Convert train and validation images into lists with locations
 train_nrrd_files = sorted([os.path.join(train_dir, f) for f in os.listdir(
     train_dir) if f.endswith(".nrrd") and not f.endswith(".seg.nrrd")])
 train_seg_nrrd_files = sorted([os.path.join(train_dir, f)
-                               for f in os.listdir(train_dir) if f.endswith(".seg.nrrd")])
-# validation image files
+                              for f in os.listdir(train_dir) if f.endswith(".seg.nrrd")])
 val_nrrd_files = sorted([os.path.join(val_dir, f) for f in os.listdir(
     val_dir) if f.endswith(".nrrd") and not f.endswith(".seg.nrrd")])
 val_seg_nrrd_files = sorted([os.path.join(val_dir, f)
-                             for f in os.listdir(val_dir) if f.endswith(".seg.nrrd")])
+                            for f in os.listdir(val_dir) if f.endswith(".seg.nrrd")])
 
-# Create a list of dictionaries containing the file paths
 train_datalist = [{"image": img, "label": lbl}
                   for img, lbl in zip(train_nrrd_files, train_seg_nrrd_files)]
 validation_datalist = [{"image": img, "label": lbl}
                        for img, lbl in zip(val_nrrd_files, val_seg_nrrd_files)]
 
-# Print the datalist to verify
-print(validation_datalist[0])
-
 # %%
-# Binarizing function
+# Define transforms for training and validation
+
+
 def binarize_label(label):
     return (label > 0).astype(label.dtype)
 
-#Thresholding function
+
 def threshold_image(image):
     return np.where(image < 20, 0, image)
 
 
-# Transforms
-train_transforms = Compose(
-    [
-        LoadImaged(keys=["image", "label"]),
-        EnsureChannelFirstd(keys=["image", "label"]),
-        ScaleIntensityd(keys=["image"], minv=0, maxv=255),
-        LambdaD(keys="label", func=binarize_label),
-        LambdaD(keys="image", func=threshold_image),
-        CropForegroundd(keys=["image", "label"], source_key="image"),
-        SpatialPadd(keys=["image", "label"], spatial_size=(64, 64, 64)),
-        RandSpatialCropSamplesd(keys=["image", "label"], roi_size=(
-            64, 64, 64), random_size=False, num_samples=2),
-        RandFlipd(
-            keys=["image", "label"],
-            spatial_axis=[0],
-            prob=0.10,
-        ),
-        RandFlipd(
-            keys=["image", "label"],
-            spatial_axis=[1],
-            prob=0.10,
-        ),
-        RandFlipd(
-            keys=["image", "label"],
-            spatial_axis=[2],
-            prob=0.10,
-        ),
-        RandRotate90d(
-            keys=["image", "label"],
-            prob=0.10,
-            max_k=3,
-        ),
-        RandShiftIntensityd(
-            keys=["image"],
-            offsets=0.10,
-            prob=0.50,
-        ),
-        ToTensord(keys=["image", "label"]),
-    ]
-)
+train_transforms = Compose([
+    LoadImaged(keys=["image", "label"]),
+    EnsureChannelFirstd(keys=["image", "label"]),
+    ScaleIntensityd(keys=["image"], minv=0, maxv=255),
+    LambdaD(keys="label", func=binarize_label),
+    LambdaD(keys="image", func=threshold_image),
+    CropForegroundd(keys=["image", "label"], source_key="image"),
+    SpatialPadd(keys=["image", "label"], spatial_size=(64, 64, 64)),
+    RandSpatialCropSamplesd(keys=["image", "label"], roi_size=(
+        64, 64, 64), random_size=False, num_samples=2),
+    RandFlipd(keys=["image", "label"], spatial_axis=[0], prob=0.10),
+    RandFlipd(keys=["image", "label"], spatial_axis=[1], prob=0.10),
+    RandFlipd(keys=["image", "label"], spatial_axis=[2], prob=0.10),
+    RandRotate90d(keys=["image", "label"], prob=0.10, max_k=3),
+    RandShiftIntensityd(keys=["image"], offsets=0.10, prob=0.50),
+    ToTensord(keys=["image", "label"]),
+])
 
-# Validation transforms
-val_transforms = Compose(
-    [
-        LoadImaged(keys=["image", "label"]),
-        EnsureChannelFirstd(keys=["image", "label"]),
-        ScaleIntensityd(keys=["image"], minv=-1.0, maxv=1.0),
-        LambdaD(keys="label", func=binarize_label),
-        CropForegroundd(keys=["image", "label"], source_key="image"),
-        ToTensord(keys=["image", "label"]),
-    ]
-)
-
+val_transforms = Compose([
+    LoadImaged(keys=["image", "label"]),
+    EnsureChannelFirstd(keys=["image", "label"]),
+    ScaleIntensityd(keys=["image"], minv=-1.0, maxv=1.0),
+    LambdaD(keys="label", func=binarize_label),
+    CropForegroundd(keys=["image", "label"], source_key="image"),
+    ToTensord(keys=["image", "label"]),
+])
 
 # %%
-#Dataloaders
-
-train_ds = CacheDataset(
-    data=train_datalist,
-    transform=train_transforms,
-    cache_num=24,
-    cache_rate=1.0,
-    num_workers=2,
-)
+# Create DataLoaders for training and validation
+train_ds = CacheDataset(data=train_datalist, transform=train_transforms,
+                        cache_num=24, cache_rate=1.0, num_workers=2)
 train_loader = DataLoader(train_ds, batch_size=1,
                           shuffle=True, num_workers=4, pin_memory=True)
+
 val_ds = CacheDataset(data=validation_datalist, transform=val_transforms,
                       cache_num=6, cache_rate=1.0, num_workers=4)
 val_loader = DataLoader(val_ds, batch_size=1,
                         shuffle=False, num_workers=4, pin_memory=True)
-print(f"Memmory summary {torch.cuda.memory_summary()}")
 
 # %%
-# Network 
-# current GPU cannot handle this
-
+# Define and setup the model
 model = UNETR(
     in_channels=1,
     out_channels=2,
@@ -188,28 +173,27 @@ model = UNETR(
     pos_embed="conv",
     norm_name="instance",
     res_block=True,
-    dropout_rate=0.0,
+    dropout_rate=dropout,
 )
 model = model.to(device)
 
-# Load ViT backbone weights into UNETR
-if use_pretrained is True:
-    print("Loading Weights from the Path {}".format(pretrained_path))
-    vit_dict = torch.load(pretrained_path)
+# Load pretrained weights if specified
+if use_pretrained:
+    print(f"Loading Weights from the Path {pretrained_path}")
+    vit_dict = torch.load(pretrained_path, weights_only=True)
     vit_weights = vit_dict["state_dict"]
     model_dict = model.vit.state_dict()
-
     vit_weights = {k: v for k, v in vit_weights.items() if k in model_dict}
     model_dict.update(vit_weights)
     model.vit.load_state_dict(model_dict)
-    del model_dict, vit_weights, vit_dict
-    print("Pretrained Weights Succesfully Loaded !")
-
-elif use_pretrained is False:
-    print("No weights were loaded, all weights being used are randomly initialized!")
+    print("Pretrained Weights Successfully Loaded!")
+else:
+    print("No weights were loaded, all weights are randomly initialized!")
 
 model.to(device)
 
+# %%
+# Setup loss function, optimizer, and metrics
 loss_function = DiceCELoss(to_onehot_y=True, softmax=True)
 torch.backends.cudnn.benchmark = True
 optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=1e-5)
@@ -225,7 +209,10 @@ epoch_loss_values = []
 metric_values = []
 
 # %%
-def validation(epoch_iterator_val):
+# %% Define the validation component
+
+
+def validation(epoch_iterator_val, dice_val_best):
     model.eval()
     dice_vals = []
 
@@ -250,11 +237,14 @@ def validation(epoch_iterator_val):
         dice_metric.reset()
 
     mean_dice_val = np.mean(dice_vals)
+    if mean_dice_val > dice_val_best:
+        print(f"validation output shapes {val_outputs.shape}")
     # Log validation dice score
     mlflow.log_metric('val_dice', mean_dice_val, step=global_step)
 
     return mean_dice_val
-
+    #%% Define training function
+from PIL import Image
 
 def train(global_step, train_loader, dice_val_best, global_step_best):
     model.train()
@@ -274,14 +264,14 @@ def train(global_step, train_loader, dice_val_best, global_step_best):
         epoch_iterator.set_description(
             "Training (%d / %d Steps) (loss=%2.5f)" % (global_step, max_iterations, loss))
 
-        #Log metric loss
+        # Log metric loss
         mlflow.log_metric('train_loss', loss.item(),
-                  step=global_step)  # Log training loss
+                          step=global_step)  # Log training loss
 
         if (global_step % eval_num == 0 and global_step != 0) or global_step == max_iterations:
             epoch_iterator_val = tqdm(
                 val_loader, desc="Validate (X / X Steps) (dice=X.X)", dynamic_ncols=True)
-            dice_val = validation(epoch_iterator_val)
+            dice_val = validation(epoch_iterator_val, dice_val_best)
 
             epoch_loss /= step
             epoch_loss_values.append(epoch_loss)
@@ -294,10 +284,18 @@ def train(global_step, train_loader, dice_val_best, global_step_best):
                         dice_val_best, dice_val))
                 torch.save(model.state_dict(),
                            os.path.join(logdir, experiment_name + ".pth"))
-                print(
-                    "Model Was Saved ! Current Best Avg. Dice: {} Current Avg. Dice: {}".format(
-                        dice_val_best, dice_val)
-                )
+                model.eval()
+                with torch.no_grad():
+                    seg_output = sliding_window_inference(
+                        x, (64, 64, 64), 1, model)
+                    image_slice = seg_output[0, 0, :, :, 35].cpu().numpy() > 0.2
+                    #image_slice = (image_slice * 255).astype(np.uint8)
+                    image = Image.fromarray(image_slice)
+                    image_path = os.path.join(
+                        logdir, str(dice_val) + "_output_slice.png")
+                    image.save(image_path)
+                    mlflow.log_artifact(image_path)
+                    print(f"Here is the shape of output {x.shape}")
             else:
                 print(
                     "Model Was Not Saved ! Current Best Avg. Dice: {} Current Avg. Dice: {}".format(
@@ -308,25 +306,20 @@ def train(global_step, train_loader, dice_val_best, global_step_best):
         global_step += 1
     return global_step, dice_val_best, global_step_best
 
-# %%
+    # %%
 # Start MLflow run
-import mlflow
-
 mlflow.set_tracking_uri("file:./mlruns")
+
 # Check if the experiment already exists
 experiment = mlflow.get_experiment_by_name(experiment_name)
 if experiment is None:
-    # If the experiment does not exist, create it
     experiment_id = mlflow.create_experiment(experiment_name)
 else:
-    # If the experiment exists, get its ID
     experiment_id = experiment.experiment_id
-# Set the experiment as the active experiment
 mlflow.set_experiment(experiment_name)
 
 with mlflow.start_run() as run:
     while global_step < max_iterations:
-        mlflow.log_param('start_val', 1.0)
         global_step, dice_val_best, global_step_best = train(
             global_step, train_loader, dice_val_best, global_step_best)
 
@@ -344,9 +337,3 @@ with mlflow.start_run() as run:
 
 print(
     f"train completed, best_metric: {dice_val_best:.4f} " f"at iteration: {global_step_best}")
-
-
-# %%
-
-
-
