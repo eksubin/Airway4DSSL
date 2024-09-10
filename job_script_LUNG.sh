bbaba21be07613c6a5a85d815e9455d6e281a53f
Subin Erattakulangara <subinedu@gmail.com>
2024-09-10 10:06:31 -0500

    job updates

--- job_script_LUNG.sh
+++ job_script_LUNG.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#$ -N FourDSeg         # Name of the job
+#$ -N FourDSegFrench         # Name of the job
 #$ -q LUNG                   # Queue name
 #$ -pe smp 16               # Number of slots (cores)
 #$ -l gpu_a100=true         # Number of GPUs
 #$ -cwd                     # Use the current working directory
-#$ -o ./logs/outputLUNG.log            # Output log file
-#$ -e ./logs/errorLUNG.log             # Error log file
+#$ -o ./logs/outputLUNGFrench.log            # Output log file
+#$ -e ./logs/errorLUNGFrench.log             # Error log file
 
 conda activate unet3D
 export CUDA_VISIBLE_DEVICES=0
@@ -14,4 +14,6 @@ export CUDA_VISIBLE_DEVICES=0
 #python MLFlowFineTune.py --max_iterations=50000 --dropout=0 --experiment_name=Prototyping
 #python MLFlowFineTune.py --max_iterations=50000 --dropout=0.1 --experiment_name=Prototyping --note="replaced input image range from 0-255 to 0-1"
 #python MLFlowFineTune.py --max_iterations=100000 --dropout=0.5 --experiment_name=Prototyping --note="corrected the validation(0--1) issue"
-python MLFlowFineTuneLatestStack.py --max_iterations=20000 --dropout=0.1 --experiment_name=Test --note="3DTime stack" --pretrained_path="./mlruns/910001340442692122/a0937dd7ad2e4151b5652de14c3e2953/artifacts/models/Test_final/data/model.pth"
+#python MLFlowFineTuneLatestStack.py --max_iterations=20000 --dropout=0.1 --experiment_name=Test --note="3DTime stack" --pretrained_path="./mlruns/910001340442692122/a0937dd7ad2e4151b5652de14c3e2953/artifacts/models/Test_final/data/model.pth"
+
+
--- job_script_SGL.sh
+++ job_script_SGL.sh
@@ -15,6 +15,10 @@ export CUDA_VISIBLE_DEVICES=0
 #python MLFlowFineTune.py --max_iterations=50000 --dropout=0 --experiment_name=Prototyping
 #python MLFlowFineTuneLatest.py --max_iterations=100000 --dropout=0.1 --experiment_name=Prototyping --note="0-1 Train and 0-1 Fine tuning" --pretrained_path="./mlruns/710251981863385259/fb2a1895681f4d8098735c9e1f04e3cf/artifacts/models/TrainingFrench_final/data/model.pth"
 #python MLFlowFineTuneLatest.py --max_iterations=50000 --dropout=0.5 --experiment_name=Prototyping --note="0-1 Train and 0-1 Fine tuning" --pretrained_path="./mlruns/710251981863385259/fb2a1895681f4d8098735c9e1f04e3cf/artifacts/models/TrainingFrench_final/data/model.pth"
-python MLFlowFineTuneLatestStack.py --max_iterations=50000 --dropout=0.1 --experiment_name=Test --note="3DTime stack" --pretrained_path="./mlruns/910001340442692122/a0937dd7ad2e4151b5652de14c3e2953/artifacts/models/Test_final/data/model.pth"
+#python MLFlowFineTuneLatestStack.py --max_iterations=50000 --dropout=0.1 --experiment_name=Test --note="3DTime stack" --pretrained_path="./mlruns/910001340442692122/a0937dd7ad2e4151b5652de14c3e2953/artifacts/models/Test_final/data/model.pth"
 #python MLFlowFineTune.py --max_iterations=100000 --dropout=0.5 --experiment_name=Prototyping --note="corrected the validation(0--1) issue"
 #python SSLStackTrain.py --max_iterations=1000 --image_max=1 --image_min=0 --threshold=0.08 --model_name=Test --note=Parallel --dataset=01 
+
+
+# testing the french speaker dataset
+python MLFlowFineTuneLatestStackFrenchSpeaker.py --train_dir="./Data/VoiceUsers/Train/users32/" --val_dir="./Data/VoiceUsers/Train/user2/" --max_iterations=10000 --dropout=0.1 --experiment_name=Test --note="3DTime french speker" --use_pretrained --pretrained_path="./mlruns/910001340442692122/a0937dd7ad2e4151b5652de14c3e2953/artifacts/models/Test_final/data/model.pth"
